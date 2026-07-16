ce12da820e77a59ba586fcd1c77f13e1c4ac54c6
Naman <naman.prajapati3456@gmail.com>
2026-07-16 07:25:52 +0000

    data type

---
+++ 0_project/01_variable.js
@@ -0,0 +1,21 @@
+const accountId = 144553   // fixed value 
+let accountEmail = "naman@gmail.com" 
+var accountPassword = "12434"
+accountCity = "Jaipur"
+let accountState;
+   
+// accountId = 2
+// console.log(accountId);
+
+
+accountEmail =  "absc@gmail.com"
+accountPassword = "232425"
+accountCity = "Dehradun"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+/*
+Prefer is not issue var 
+because of issue in block scope and functional scope
+
+*/
---
+++ 0_project/02_datatype.js
@@ -0,0 +1,37 @@
+"use strict";   //treat all js code as a newer version 
+
+// alert("hello")
+// alert( 2 + 2) // we are using node js / not using browser
+
+
+console.log( 3 
+    + 
+    3);   // Code readability should be high 
+
+console.log(3+3)
+
+
+// documentation
+
+// https://tc39.es/ecma262/2023/multipage/
+//mdn
+
+
+let name = "naman"
+let age = 234
+let isloggedIn = false
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+// boolean => True / False
+// null => Standlone value     //object
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "naman")
+console.log(typeof isloggedIn)
+console.log(typeof null)
+
